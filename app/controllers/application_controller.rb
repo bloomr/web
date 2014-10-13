4ce543cb02dad48fd0eaa2aa84adf01453e2e2d4
Simon Lehericey <simon.lehericey@gmail.com>
2014-10-13 21:06:07 +0200

    add basic auth, use for staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  if ENV['BASIC_AUTH'] && ENV['BASIC_AUTH'].downcase == 'true'
+    http_basic_authenticate_with :name => ENV['BASIC_AUTH_USER'], :password => ENV['BASIC_AUTH_PASSWORD']
+  end
+
 end
