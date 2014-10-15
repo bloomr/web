8447bd51ed4f4e94d985d726e2d060c5eadf7673
Matthieu Balmes <matthieu.balmes@gmail.com>
2014-10-15 12:30:35 +0200

    Add more infos on the gallery and improve UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :questions, :allow_destroy => true
 
   acts_as_taggable_on :keywords
+
+  def love_job_answer
+    answer = nil
+    questions.each do |question|
+      if question.identifier == "love_job"
+        answer = question.answer
+      end
+    end
+    return answer
+  end
 end
