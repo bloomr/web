3907ec1bf90bc84d22536db5592984f7ae67d503
Matthieu Balmes <matthieu.balmes@gmail.com>
2014-12-08 21:42:00 +0100

    Create migration to add 'published' flag to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020205950) do
+ActiveRecord::Schema.define(version: 20141208203304) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20141020205950) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "questions", force: true do |t|
-    t.string   "title"
+    t.string   "title",                  null: false
     t.text     "answer",     limit: 255
     t.string   "identifier"
     t.datetime "created_at"
@@ -79,12 +79,12 @@ ActiveRecord::Schema.define(version: 20141020205950) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20141020205950) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "published",              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
