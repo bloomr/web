f103367b75dcd057c8737d5efb71144edc28447d
Simon Lehericey <simon.lehericey@gmail.com>
2014-12-09 17:32:17 +0100

    add tests on participer, fix tests on portraits

--- spec/features/display_portraits_spec.rb
+++ spec/features/display_portraits_spec.rb
@@ -1,21 +1,29 @@
 feature "List of portraits" do
 
   scenario "Displaying the list of portraits" do
-    visit "/portraits" do
-      user1 = User.create(
-          :email => "john.doe@example.org",
-          :first_name => "John",
-          :job_title => "Developer")
-      user2 = User.create(
-          :email => "jane.doe@example.org",
-          :first_name => "Jane",
-          :job_title => "Architect")
-      user_unemployed = User.create(
-          :email => "mike.test@example.org",
-          :first_name => "Mike"
-      )
-      expect(page).to have_content('Portraits')
-      expect(page).to have_selector('#portraits_list .portrait-item', :count => 2)
-    end
+
+    user1 = User.create!(
+        :email => "john.doe@example.org",
+        :first_name => "John",
+        :job_title => "Developer",
+        :password => "abcdfedv",
+        :published => true)
+    user2 = User.create!(
+        :email => "jane.doe@example.org",
+        :first_name => "Jane",
+        :job_title => "Architect",
+        :password => "abcdfedv",
+        :published => true)
+    user_not_published = User.create!(
+        :email => "mike.test@example.org",
+        :first_name => "Mike",
+        :password => "abcdfedv",
+        :job_title => "Architect")
+
+    visit "/portraits"
+
+    expect(page).to have_content('Galerie')
+    expect(page).to have_selector('.portrait-item', :count => 2)
+
   end
 end
---
+++ spec/features/participer_spec.rb
@@ -0,0 +1,31 @@
+feature "Enrolling" do
+
+  scenario "I can enroll" do
+
+    visit "/participer"
+
+    expect(page).to have_text("Votre email")
+    fill_in :email, :with => "loulou@yop.com"
+    fill_in 'Votre métier', :with => "jardinier"
+    fill_in 'Votre prénom', :with => "loulou"
+    fill_in "Au fond, qu'est ce qui fait que vous aimez votre métier ?", :with => "j aime les arbres"
+    fill_in "Que faites vous exactement ?", :with => "je plante des pommiers"
+    fill_in "5 mots-clés associés à votre métier, séparés par une virgule", :with => "1, 2, 3, 4, 5"
+
+    click_button "Enregistrer"
+
+    expect(page).to have_text("Merci beaucoup !")
+
+    expect(User.count).to eq(1)
+    loulou = User.first
+    expect(loulou.email).to eq("loulou@yop.com")
+    expect(loulou.job_title).to eq("jardinier")
+    expect(loulou.first_name).to eq("loulou")
+    expect(loulou.questions.count).to eq(2)
+    expect(loulou.questions[0].answer).to eq("j aime les arbres")
+    expect(loulou.questions[1].answer).to eq("je plante des pommiers")
+    expect(loulou.keyword_list).to eq(["1", "2", "3", "4", "5"])
+    expect(loulou.published).to eq(false)
+
+  end
+end
