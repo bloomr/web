db48cab1a4f7c33e792599ec16b201104e575aea
Simon Lehericey <simon.lehericey@gmail.com>
2014-12-09 14:24:03 +0100

    add keywords in participer

--- app/controllers/participer_controller.rb
+++ app/controllers/participer_controller.rb
@@ -19,7 +19,7 @@ class ParticiperController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :job_title, :first_name, questions_attributes: [:identifier, :title, :answer])
+    params.require(:user).permit(:email, :job_title, :first_name, :keyword_list, questions_attributes: [:identifier, :title, :answer])
   end
 
 end
