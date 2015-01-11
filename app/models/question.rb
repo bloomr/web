2a268d95d77acd4e87df0059953d0c3e63f50dab
Matthieu Balmes <matthieu.balmes@gmail.com>
2015-01-11 18:26:11 +0100

    Sort the questions by alphabetical identifier (and null identifiers come after non-null identifiers)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+
+  def <=> other_question
+    if identifier.length == 0
+      return +1
+    end
+
+    if other_question.identifier.length == 0
+      return -1
+      end
+
+    identifier <=> other_question.identifier
+  end
 end
