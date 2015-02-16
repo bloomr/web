2f5fce5ee89897ccaedbeb5d2bb4f86f03923a1b
Matthieu Balmes <matthieu.balmes@gmail.com>
2015-02-16 12:05:05 +0100

    Start integrating the graphical chart

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -6,6 +6,7 @@ class TagController < ApplicationController
       params[:page] = 1
     end
     @tag = params[:id]
+    @keyword = Keyword.find_by(:tag => @tag)
     @portraits = User.joins(:keywords).where("users.published = ? and users.job_title IS NOT NULL AND keywords.tag = ?", true, @tag).select("distinct users.*").limit(12).offset(12* (params[:page]-1)).order(updated_at: :desc)
   end
 
