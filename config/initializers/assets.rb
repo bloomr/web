5f1b55dc33bffb37628bfa51e0de0c960826d2bd
Simon Lehericey <simon.lehericey@gmail.com>
2015-04-30 18:31:17 +0200

    remove uservoice and addthis

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( user_voice.js search.js )
+Rails.application.config.assets.precompile += %w( search.js )
