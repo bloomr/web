7a0ed4cfd8c70281eab62f1beee979338a788f81
Simon Lehericey <simon.lehericey@gmail.com>
2015-05-21 00:13:37 +0200

    add admin gestion of question comment

---
+++ app/admin/question_comment.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register QuestionComment do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  permit_params :comment, :author_name, :author_avatar_url
+
+end
