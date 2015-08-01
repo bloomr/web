c7bc039c06f6877d2edb5e4756e3a31a751891c0
Simon Lehericey <simon.lehericey@gmail.com>
2015-08-01 13:56:25 +0200

    fix comment api test url

--- spec/api/users_controller_spec.rb
+++ spec/api/users_controller_spec.rb
@@ -28,19 +28,19 @@ RSpec.describe Api::V1::UsersController, :type => :request do
 
   describe "POST #users" do
     it "responds unauthorized with an HTTP 401 status code if bad api key" do
-      post "api/v1/users", {}, { "Accept" => "application/json" }
+      post "/api/v1/users", {}, { "Accept" => "application/json" }
       expect(response).to have_http_status(401)
       expect(JSON.parse(response.body)["error"]).to eq("invalid key")
     end
 
     it "responds yop with 200 code if creation of user" do
-      post "api/v1/users", payload, { "Accept" => "application/json" }
+      post "/api/v1/users", payload, { "Accept" => "application/json" }
       expect(JSON.parse(response.body)["user_id"]).to eq(1)
       expect(response).to have_http_status(200)
     end
 
     it "create the user correctly" do
-      post "api/v1/users", payload, { "Accept" => "application/json" }
+      post "/api/v1/users", payload, { "Accept" => "application/json" }
       user = User.find(1)
       expect(user.email).to eq("yopyop@yop.com")
       expect(user.keywords.map{ |k| k.tag }).to eq(["tag1", "tag2", "tag3"])
@@ -54,7 +54,7 @@ RSpec.describe Api::V1::UsersController, :type => :request do
           :password => "abcdfedv",
           :published => true)
 
-      post "api/v1/users", payload, { "Accept" => "application/json" }
+      post "/api/v1/users", payload, { "Accept" => "application/json" }
       expect(response).to have_http_status(400)
       expect(JSON.parse(response.body)["error"]).to eq("creation ko")
     end
@@ -92,7 +92,7 @@ RSpec.describe Api::V1::UsersController, :type => :request do
           :published => true)
 
 
-      patch "api/v1/users/" + user.id.to_s, question, { "Accept" => "application/json" }
+      patch "/api/v1/users/" + user.id.to_s, question, { "Accept" => "application/json" }
       user = User.find(1)
       expect(user.questions.length).to eq(2)
       expect(user.questions.map{ |k| k.identifier }).to eq(["a", "b"])
@@ -118,7 +118,7 @@ RSpec.describe Api::V1::UsersController, :type => :request do
           ],
           :published => true)
 
-      patch "api/v1/users/" + user.id.to_s, question, { "Accept" => "application/json" }
+      patch "/api/v1/users/" + user.id.to_s, question, { "Accept" => "application/json" }
       expect(response).to have_http_status(400)
       expect(JSON.parse(response.body)["error"]).to eq("update ko")
     end
