0f498d525ee33f85b0e6fe26b700275f3bba44f7
Simon Lehericey <simon.lehericey@gmail.com>
2015-09-21 19:43:21 +0200

    ruby: go for 2.2.3
    to stop memory leaks from 2.1.0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906200202) do
+ActiveRecord::Schema.define(version: 20150920214622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
