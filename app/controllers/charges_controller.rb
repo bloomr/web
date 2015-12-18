5d95cdecec34914b48bcedc1e042176b31ec21cd
Simon Lehericey <simon.lehericey@gmail.com>
2015-12-18 01:18:03 +0100

    fix mail in confirmation page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -15,6 +15,8 @@ class ChargesController < ApplicationController
     :currency    => 'eur'
   )
 
+  @email = params[:stripeEmail];
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
