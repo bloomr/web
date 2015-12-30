f6e649f8bec828ec9a83d55534cbe21fbba7561b
Simon Lehericey <simon.lehericey@gmail.com>
2015-12-30 18:23:16 +0100

    add smtp configuration

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,15 @@ module Bloomr
                  :max_age => 86400
       end
     end
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              ENV['SMTP_ADDRESS'],
+      port:                 587,
+      domain:               ENV['SMTP_DOMAIN'],
+      user_name:            ENV['SMTP_USER_NAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
   end
 end
