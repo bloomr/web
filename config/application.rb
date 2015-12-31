9ad5c3e1e85313c7670cd85a879ad750bf7debeb
Simon Lehericey <simon.lehericey@gmail.com>
2015-12-31 17:34:38 +0100

    devise: fr localization and style

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Bloomr
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fr
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
