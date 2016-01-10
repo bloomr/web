e248c2bffea2c7a32608d79fc8ef90a55ffaeae5
Simon Lehericey <simon.lehericey@gmail.com>
2016-01-10 19:39:33 +0100

    connexion: wording according to laurent's feedback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'bloomrs_robot@bloomr.org'
+  config.mailer_sender = 'alfred@bloomr.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
