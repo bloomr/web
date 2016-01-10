cd54674bd42180953dd8e148b57de4a9355828ee
Simon Lehericey <simon.lehericey@gmail.com>
2016-01-10 19:40:33 +0100

    profile: answer display fit the content length

--- app/assets/javascripts/me.js
+++ app/assets/javascripts/me.js
@@ -5,9 +5,21 @@
       var toolbar = $('.toolbar', el)[0];
       new wysihtml5.Editor(editor, {
         toolbar: toolbar,
-          parserRules:  wysihtml5ParserRules
+        parserRules:  wysihtml5ParserRules
+      });
+    });
+
+    $('form').on('submit', function() {
+      $('.editor').each(function(i, div) {
+        var $div = $(div);
+        $('<input />').attr('type', 'hidden')
+                  .attr('name', $div.attr('name'))
+                  .attr('value', $div.html())
+                  .appendTo('form');
+        return true;
       });
     });
   };
+
   $(document).on('page:change', make_it_editable);
 })();
