a94d53b4b078f71d6c9d22bc0281caf211aa39e0
Simon Lehericey <simon.lehericey@gmail.com>
2016-03-08 14:59:53 +0100

    https: remove previous https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
-  if ENV['BASIC_AUTH'] && ENV['BASIC_AUTH'].downcase == 'true'
-    http_basic_authenticate_with :name => ENV['BASIC_AUTH_USER'], :password => ENV['BASIC_AUTH_PASSWORD']
-  end
-
-  def https_enabled?
-    #active by default
-    ENV['ONLY_HTTP_ENABLE'] != 'true'
-  end
-
   def record_not_found
     render :text => "404 Not Found", :status => 404
   end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -139,7 +139,7 @@ ActiveAdmin.setup do |config|
   # You can add before, after and around filters to all of your
   # Active Admin resources and pages from here.
   #
-  config.before_filter :force_ssl_redirect, if: :https_enabled?
+  #config.before_filter :force_ssl_redirect
 
 
   # == Setting a Favicon
