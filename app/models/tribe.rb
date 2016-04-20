5f8986d033112cf95640ffe7e6eaf62b71e3b7a4
Simon Lehericey <simon.lehericey@gmail.com>
2016-04-20 11:49:16 +0200

    tribes: add last_month_view_count

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -3,6 +3,16 @@ class Tribe < ActiveRecord::Base
   has_many :keywords
   before_save :normalize_name
 
+  def last_month_view_count
+    user_ids = self.users.pluck(:id)
+    Impression
+      .where(impressionable_type:'User', impressionable_id: user_ids,
+            created_at: Date.today.beginning_of_month.last_month..Date.today.beginning_of_month)
+      .select(:impressionable_id, :request_hash)
+      .distinct
+      .count(:impressionable_id, :request_hash)
+  end
+
   private
 
   def normalize_name
--- spec/models/tribe_spec.rb
+++ spec/models/tribe_spec.rb
@@ -9,4 +9,44 @@ RSpec.describe Tribe, :type => :model do
       end
     end
   end
+
+  describe '#last_month_view_count' do
+    context 'when a user with 1 view last month is in the tribe' do
+      let(:tribe) { Tribe.create( name: '3.0') }
+      before do
+        impressions = []
+        impressions << Impression.new(created_at: Date.today.beginning_of_month.last_month, request_hash: 'yop')
+        impressions << Impression.new(created_at: Date.today.beginning_of_month.last_month - 1, request_hash: 'tip')
+        create(:user, impressions: impressions, tribes: [tribe])
+      end
+
+      it 'returns one view' do
+        expect(tribe.last_month_view_count).to eq(1)
+      end
+
+      context 'with another user with 1 view last month in the tribe' do
+        before do
+          impressions = []
+          impressions << Impression.new(created_at: Date.today.beginning_of_month.last_month, request_hash: 'yop')
+          create(:user, impressions: impressions, tribes: [tribe])
+        end
+
+        it 'returns the 2 views' do
+          expect(tribe.last_month_view_count).to eq(2)
+        end
+      end
+
+      context 'with an user with 1 view last month not in the tribe' do
+        before do
+          impressions = []
+          impressions << Impression.new(created_at: Date.today.beginning_of_month.last_month, request_hash: 'yop')
+          create(:user, impressions: impressions)
+        end
+
+        it 'returns one view' do
+          expect(tribe.last_month_view_count).to eq(1)
+        end
+      end
+    end
+  end
 end
