9b746eaa827ccd7059326dc803619b7f7feb6c85
Simon Lehericey <simon.lehericey@gmail.com>
2016-04-23 00:12:22 +0200

    tribes: display bloomeurs

--- app/controllers/tribes_controller.rb
+++ app/controllers/tribes_controller.rb
@@ -1,6 +1,9 @@
 class TribesController < ApplicationController
   def index
     @tribes = Tribe.all
+    @page = params[:page].to_i
+    @portraits = User.find_published_with_love_job_question page: @page
+    @popular_keywords = Keyword.popular_keywords
   end
 
   def show
