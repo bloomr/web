67fda949b46ab01c418d0eb42735647d9622d8cb
Simon Lehericey <simon.lehericey@gmail.com>
2016-04-28 18:23:42 +0200

    questions: remove custom comparison

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,18 +6,6 @@ class Question < ActiveRecord::Base
 
   before_save :strip_injection_from_answer
 
-  def <=> other_question
-    if position.length == 0
-      return +1
-    end
-
-    if other_question.position.length == 0
-      return -1
-    end
-
-    position <=> other_question.position
-  end
-
   def published_questions
     question_comments.select {|q| q.published }
   end
