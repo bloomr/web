65fa4b7fcd76c57607ce6264656288eb8dc9c04c
Simon Lehericey <simon.lehericey@gmail.com>
2016-04-30 16:47:14 +0200

    tests: add portraits details integration test

--- spec/features/display_portraits_spec.rb
+++ spec/features/display_portraits_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 feature 'List of portraits' do
 
-  scenario 'Displaying the list of portraits' do
-     (1..2).each { |i| FactoryGirl.create(:user_published_with_questions, email: "#{i}@a.com") }
-     (3..4).each { |i| FactoryGirl.create(:user, email: "#{i}@a.com") }
+  let!(:user1) { FactoryGirl.create(:user_published_with_questions, email: '1@a.com') }
 
+  before do
+    FactoryGirl.create(:user_published_with_questions, email: '2@a.com')
+    (3..4).each { |i| FactoryGirl.create(:user, email: "#{i}@a.com") }
+  end
+
+  scenario 'Displaying the list of portraits' do
     visit '/tribes'
 
     expect(page).to have_content('les bloomeurs')
     expect(page).to have_selector('#portraits_list > li', count: 3)
   end
+
+  scenario 'Displaying one portrait details' do
+    visit "/portraits/#{user1.id}"
+  end
+
 end
