f4eb5f404e8d49fab57eaaccbb5d957b18623c01
Simon Lehericey <simon.lehericey@gmail.com>
2016-05-13 16:53:03 +0200

    sujetdebac: change link to paypal accordingly

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -6,6 +6,7 @@ feature 'Buy a journey' do
 
     visit '/payment'
     expect(page).not_to have_button('Régler mes 9.90€')
+    expect(page).not_to have_css('a[href="https://paypal.me/bloomr/9.9"]')
   end
 
   scenario 'I get a discount if I come from a partner' do
@@ -13,6 +14,7 @@ feature 'Buy a journey' do
     visit '/partner/sujetdubac'
     visit '/payment'
     expect(page).to have_button('Régler mes 9.90€')
+    expect(page).to have_css('a[href="https://paypal.me/bloomr/9.9"]')
   end
 end
 
