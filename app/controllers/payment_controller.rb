4c42a5ec13683cdc841b10a7ddc415336409c6b1
Simon Lehericey <simon.lehericey@gmail.com>
2016-05-19 23:25:50 +0200

    Bloomy: save bloomy and add to mailchimp

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -4,28 +4,9 @@ class PaymentController < ApplicationController
   end
 
   def create
-    bloomie = params[:first_name] + ' - ' + params[:age] + ' ans - ' + params[:email]
-
-    # Amount in cents
-    if cookies[:sujetdubac]
-      amount = 990
-      metadata = { 'info_client' => bloomie, 'source' => 'sujetdubac' }
-    else
-      amount = 3500
-      metadata = { 'info_client' => bloomie }
-    end
-
-    token = params[:stripeToken]
-
-    Stripe::Charge.create(
-      amount: amount,
-      currency: 'eur',
-      source: token,
-      description: '1 Parcours Bloomr',
-      receipt_email: params[:email],
-      metadata: metadata
-    )
-
+    bloomy = Bloomy.create!(bloomy_params.merge(password: 'totototo'))
+    charge(bloomy)
+    subscribe_to_mailchimp(bloomy)
     redirect_to payment_thanks_path
 
   rescue Stripe::CardError => e
@@ -35,5 +16,50 @@ class PaymentController < ApplicationController
 
   def thanks
     render layout: 'home'
+  end
+
+  private
+
+  def bloomy_params
+    params.require(:bloomy).permit(:email, :first_name, :age)
+  end
+
+  def charge(bloomy)
+    Stripe::Charge.create(
+      amount: amount,
+      currency: 'eur',
+      source: params[:stripeToken],
+      description: '1 Parcours Bloomr',
+      receipt_email: bloomy.email,
+      metadata: metadata(bloomy)
+    )
+  end
+
+  def amount
+    return 990 if cookies[:sujetdubac]
+    3500
+  end
+
+  def metadata(bloomy)
+    bloomy_s = "#{bloomy.first_name} - #{bloomy.age} ans - #{bloomy.email}"
+    metadata = { 'info_client' => bloomy_s }
+    metadata['source'] = 'sujetdubac' if cookies[:sujetdubac]
+    metadata
+  end
+
+  def subscribe_to_mailchimp(bloomy)
+    return if ENV['MAILCHIMP_ACTIVATED'].nil?
+
+    headers = { 'Authorization' => "apikey #{ENV['MAILCHIMP_API_KEY']}",
+                'Content-Type'  => 'application/json' }
+
+    body = { 'status' => 'subscribed', 'email_address' => bloomy.email,
+             'merge_fields' =>
+             { 'FNAME' => bloomy.first_name, 'MMERGE3' => bloomy.age }
+           }.to_json
+
+    url = 'https://us9.api.mailchimp.com/3.0/lists/9ec70e12ca/members'
+
+    HTTParty.post(url, headers: headers, body: body)
   end
 end
--- spec/controllers/payment_controller_spec.rb
+++ spec/controllers/payment_controller_spec.rb
@@ -16,37 +16,104 @@ RSpec.describe PaymentController, type: :controller do
   end
 
   describe 'POST #create' do
-    context 'with no cookie' do
-      it 'charge the right amount and redirect to payment_thanks' do
-        allow(Stripe::Charge).to receive(:create).with(amount: 3500,
-                                                       currency: 'eur',
-                                                       source: '1234',
-                                                       description: '1 Parcours Bloomr',
-                                                       receipt_email: 'loulou@lou.com',
-                                                       metadata: { 'info_client' => 'loulou - 44 ans - loulou@lou.com' })
+    let(:bloomy) do
+      { first_name: 'loulou', email: 'loulou@lou.com', age: '44' }
+    end
+    let(:payload) { { bloomy: bloomy, stripeToken: '1234' } }
 
-        post :create, first_name: 'loulou', email: 'loulou@lou.com', age: '44', stripeToken: '1234'
+    before do
+      allow(HTTParty).to receive(:post)
+      allow(Stripe::Charge).to receive(:create)
+    end
 
-        expect(response).to redirect_to(payment_thanks_path)
+    describe 'the bloomy creation' do
+      subject { Bloomy.first }
+
+      before do
+        post :create, payload
+      end
+
+      it { expect(subject.first_name).to eq('loulou') }
+      it { expect(subject.email).to eq('loulou@lou.com') }
+      it { expect(subject.age).to eq(44) }
+    end
+
+    describe 'the stripe charges' do
+      let(:stripes_args) do
+        { amount: amount, currency: 'eur', source: '1234',
+          description: '1 Parcours Bloomr',
+          receipt_email: 'loulou@lou.com', metadata: metadata }
+      end
+
+      context 'with no cookie' do
+        let(:metadata) do
+          { 'info_client' => 'loulou - 44 ans - loulou@lou.com' }
+        end
+        let(:amount) { 3500 }
+
+        it 'charges the right amount and redirect to payment_thanks' do
+          expect(Stripe::Charge).to receive(:create).with(stripes_args)
+          post :create, payload
+          expect(response).to redirect_to(payment_thanks_path)
+        end
+      end
+
+      context 'with sujetdubac cookie' do
+        let(:metadata) do
+          { 'info_client' => 'loulou - 44 ans - loulou@lou.com',
+            'source' => 'sujetdubac' }
+        end
+        let(:amount) { 990 }
+
+        it 'charge the right amount,
+            set the right source and redirect to payment_thanks' do
+          request.cookies[:sujetdubac] = true
+          expect(Stripe::Charge).to receive(:create).with(stripes_args)
+          post :create, payload
+          expect(response).to redirect_to(payment_thanks_path)
+        end
       end
     end
 
-    context 'with sujetdubac cookie' do
-      it 'charge the right amount, set the right source and redirect to payment_thanks' do
-        allow(Stripe::Charge).to receive(:create).with(amount: 990,
-                                                       currency: 'eur',
-                                                       source: '1234',
-                                                       description: '1 Parcours Bloomr',
-                                                       receipt_email: 'loulou@lou.com',
-                                                       metadata: {
-                                                         'info_client' => 'loulou - 44 ans - loulou@lou.com',
-                                                         'source' => 'sujetdubac'
-                                                       })
+    describe 'the mailchimp inscription' do
+      let(:url) { 'https://us9.api.mailchimp.com/3.0/lists/9ec70e12ca/members' }
 
-        request.cookies[:sujetdubac] = true
-        post :create, first_name: 'loulou', email: 'loulou@lou.com', age: '44', stripeToken: '1234'
+      let(:headers) do
+        { 'Authorization' => 'apikey api',
+          'Content-Type' => 'application/json' }
+      end
 
-        expect(response).to redirect_to(payment_thanks_path)
+      let(:body) do
+        { 'status' => 'subscribed', 'email_address' => 'loulou@lou.com',
+          'merge_fields' => { 'FNAME' => 'loulou', 'MMERGE3' => 44 } }.to_json
+      end
+
+      let(:post_body) { { headers: headers, body: body } }
+
+      before do
+        ENV['MAILCHIMP_API_KEY'] = 'api'
+      end
+
+      context 'when mailchimp option is activated' do
+        before :each do
+          ENV['MAILCHIMP_ACTIVATED'] = 'true'
+        end
+
+        it 'calls the mailchimp api' do
+          expect(HTTParty).to receive(:post).with(url, post_body)
+          post :create, payload
+        end
+      end
+
+      context 'when mailchimp option is not activated' do
+        before :each do
+          ENV['MAILCHIMP_ACTIVATED'] = nil
+        end
+
+        it 'calls the mailchimp api' do
+          expect(HTTParty).to receive(:post).with(any_args).exactly(0).times
+          post :create, payload
+        end
       end
     end
   end
