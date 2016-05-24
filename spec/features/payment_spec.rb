d08ca0a69824b55e0001f1fdf46e805346f2151a
laurent morel <laurent.marc.morel@gmail.com>
2016-05-24 18:24:45 +0200

    promotion : fix payment feature test

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 
 feature 'Buy a journey' do
 
+  before do
+    FactoryGirl.create(:campaign, :partner => 'default', :price => '35.0')
+    FactoryGirl.create(:campaign, :partner => 'sujetdubac', :price => '13.0')
+  end
+
   scenario 'Usually, I pay the full price' do
 
     visit '/payment'
-    expect(page).not_to have_button('Régler mes 9.90€')
-    expect(page).not_to have_css('a[href="https://paypal.me/bloomr/9.9"]')
+    expect(page).to have_button('Régler mes 35.00 €')
+    expect(page).to have_css('a[href="https://paypal.me/bloomr/35.00"]')
   end
 
   scenario 'I get a discount if I come from a partner' do
 
     visit '/partner/sujetdubac'
     visit '/payment'
-    expect(page).to have_button('Régler mes 9.90€')
-    expect(page).to have_css('a[href="https://paypal.me/bloomr/9.9"]')
+    expect(page).to have_button('Régler mes 13.00 €')
+    expect(page).to have_css('a[href="https://paypal.me/bloomr/13.00"]')
   end
 end
 
