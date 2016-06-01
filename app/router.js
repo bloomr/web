776006d6963dc609d77c2edc4e75e83dc2ce49b9
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-01 23:18:07 +0200

    Profile: add routes to challenges interview whatsnew

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('whatsnew');
+  this.route('interview');
+  this.route('challenges');
 });
 
 export default Router;
