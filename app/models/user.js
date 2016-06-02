6693e618a0b01b90178827d237be8d3da0255b0f
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-02 13:05:46 +0200

    Me: add user and question Model

---
+++ app/models/user.js
@@ -0,0 +1,9 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  jobTitle: attr('string'),
+  firstName: attr('string'),
+  questions: hasMany('question')
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() { return {"id":12,"first_name":"Simon","job_title":"Consultant en Informatique"}; }
+  model() { 
+    return this.store.findRecord('user', 12);
+    // return {"id":12,"first_name":"Simon","job_title":"Consultant en Informatique"}; 
+    }
 });
