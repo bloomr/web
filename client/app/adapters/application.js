6cd87efa52c73a5be8ad4fd8e3a5c699b4e2474a
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-06 23:48:49 +0200

    Ember: remove host option in favor of ember-cli proxy

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,6 +1,5 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-   namespace: 'api/v1',
-   host: 'http://localhost:3000'
+   namespace: 'api/v1'
 });
