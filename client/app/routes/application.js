a835e9516dc283f96ea186dc3a875f85df6f4a16
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-08 12:13:10 +0200

    Me: Load user with its tribes and questions

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() { 
     return Ember.RSVP.hash({
-      user: this.store.findRecord('user', 12),
+      user: this.store.findRecord('user', 12, { 'include': 'tribes,questions' }),
       tribes: this.store.findAll('tribe')
     });
   }
