d1ce6811010362a48743c1a6abef487ce3860d2c
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-08 12:06:53 +0200

    Me2: send cookie to identify user and authenticate

--- app/controllers/me2_controller.rb
+++ app/controllers/me2_controller.rb
@@ -1,5 +1,8 @@
 class Me2Controller < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    cookies['id'] = { value: current_user.id, expires: 30.day.from_now }
     render file: 'public/ember.html', layout: false
   end
 end
