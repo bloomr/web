6879de47a6e677a92156efe409f8e7dfb7b906f8
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-08 12:20:24 +0200

    Ember: Authorization reroute to /users/sign_in if unauthorized

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,5 +1,11 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-   namespace: 'api/v1'
+  namespace: 'api/v1',
+  handleResponse: function(status){
+    if(status === 403){
+      window.location = '/users/sign_in';
+    }
+    return this._super(...arguments);
+  }
 });
