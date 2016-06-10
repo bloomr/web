12bf6d339308dc2a7ac7167a46703aec7d238ae7
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-10 13:33:10 +0200

    Challenge 2: create book model

---
+++ client/app/models/book.js
@@ -0,0 +1,9 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+export default Model.extend({
+  author: attr('string'),
+  isbn: attr('number'),
+  title: attr('string'),
+  imageUrl: attr('string')
+});
