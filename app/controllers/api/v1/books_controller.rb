833672388df82072e5fe03babb3ce711a4c74c31
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-13 19:02:20 +0200

    Api Book: render simplier json for search

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -5,17 +5,13 @@ module Api
         books = Amazon::Search.books(params[:keywords])
         data = books.map do |book|
           {
-            id: book.isbn,
-            type: 'books',
-            attributes: {
-              author: book.author,
-              isbn: book.isbn,
-              title: book.title,
-              'image-url': book.image_url
-            }
+            author: book.author,
+            isbn: book.isbn,
+            title: book.title,
+            imageUrl: book.image_url
           }
         end
-        render json: { data: data }
+        render json: data
       end
     end
   end
