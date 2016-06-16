0a0ea5d4e926470359f361e6c044713e6409b925
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-16 13:22:02 +0200

    Client: challenge model: fix relation and add default properties

--- client/app/models/challenge.js
+++ client/app/models/challenge.js
@@ -1,8 +1,22 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
-  user: belongsTo('user')
+  user: hasMany('user'),
+  init() {
+    if(this.get('name') === 'must read') {
+      this.set('imageSrc', 'assets/images/profile/badge-must-read.svg');
+      this.set('description', 'Partage les bouquins à découvrir');
+      this.set('title', 'Must Read');
+      this.set('widget', 'challenge-2');
+    }
+    if(this.get('name') === 'the tribes') {
+      this.set('imageSrc', 'assets/images/profile/badge-tribu.svg');
+      this.set('description', 'Dis nous à quelle tribu tu appartiens');
+      this.set('title', 'Tribu');
+      this.set('widget', 'challenge-1');
+    }
+  }
 });
