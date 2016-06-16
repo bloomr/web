41619a42fabb43c5d5608ca0429389f72aea545a
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-16 13:20:48 +0200

    Client: challenge 1 component

--- client/app/components/challenge-1.js
+++ client/app/components/challenge-1.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service('store'),
   showIntro: true,
   showChoice: false,
   showSuccess: false,
   selectedTribes: [],
   init() {
     this._super(...arguments);
-    this.set('selectedTribes', this.get('model.user.tribes').toArray());
-    if (this.get('model.user.challenges').findBy('name','the tribes')) {
+    this.set('tribes', this.get('store').findAll('tribe'));
+    this.set('selectedTribes', this.get('user.tribes').toArray());
+    if (this.get('user.challenges').findBy('name','the tribes')) {
       this.showOnly('Success');
-    } else if (this.get('model.user.tribes').length === 0) {
+    } else if (this.get('user.tribes.length') === 0) {
       this.showOnly('Choice');
     }
   },
@@ -18,7 +20,7 @@ export default Ember.Component.extend({
     this.set('showIntro', false);
     this.set('showChoice', false);
     this.set('showSuccess', false);
-    this.set('show' + name, true); 
+    this.set('show' + name, true);
   },
   actions: {
     tribeOK() {
@@ -28,9 +30,13 @@ export default Ember.Component.extend({
       this.showOnly('Choice');
     },
     saveTribes() {
-      this.get('model.user.tribes').setObjects(this.get('selectedTribes'));
-      this.get('model.user').save();
+      this.get('user.tribes').setObjects(this.get('selectedTribes'));
       this.showOnly('Success');
+      let tribesChallenge = this.get('challenges').findBy('name', 'the tribes');
+      this.get('user.challenges').then((challenges) => {
+        challenges.pushObject(tribesChallenge);
+        this.get('user').save();
+      });
     },
   }
 });
--- client/tests/integration/components/challenge-1-test.js
+++ client/tests/integration/components/challenge-1-test.js
@@ -3,6 +3,8 @@ import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 import { clickTrigger, nativeMouseUp } from '../../helpers/ember-power-select';
 
+import { make, makeList, manualSetup, mockFindAll } from 'ember-data-factory-guy';
+
 const introSentence = "Nous avons défini ces tribus pour aider les jeunes à s'orienter.";
 const choiceSentence = "Quelle(s) tribu(s) vous conviendrai(en)t ?";
 const successSentence = 'Vous avez gagné';
@@ -10,16 +12,21 @@ const successSentence = 'Vous avez gagné';
 moduleForComponent('challenge-1', 'Integration | Component | challenge 1', {
   integration: true,
   beforeEach() {
-    let [tribe0, tribe1, tribe2] = ['tribe0', 'tribe1', 'tribe2'].map( (e, i) => Ember.Object.create({ id: i, name: e }));
-    let user = Ember.Object.create({ tribes: [tribe2], challenges: [], save: function(){} });
-    this.set('model', { tribes: [tribe0, tribe1, tribe2], user: user });
-    this.set('tribe0', tribe0);
+    manualSetup(this.container);
+    let [tribe0, tribe1, tribe2] = makeList('tribe', 3);
+    mockFindAll('tribe').returns({models:[tribe0, tribe1, tribe2]});
+    let user2 = make('user', {tribes: [tribe2]});
+    this.set('user', user2);
+    //do not know how to do otherwise
+    user2.save = () => {};
+    let challenge = make('challenge', {name: 'the tribes'});
+    this.set('challenges', Ember.A([challenge]));
     this.tribe2 = tribe2;
-  }
+  },
 });
 
 test('a user can change its tribes', function(assert) {
-  this.render(hbs`{{challenge-1 model=model}}`);
+  this.render(hbs`{{challenge-1 user=user challenges=challenges}}`);
 
   assert.ok(this.$().text().includes(introSentence));
   
@@ -32,34 +39,38 @@ test('a user can change its tribes', function(assert) {
   nativeMouseUp('.ember-power-select-option[data-option-index="0"]');
 
   this.$('.save').click();
-  assert.equal(this.get('model.user.tribes').length, 2);
-  assert.ok(this.get('model.user.tribes').indexOf(this.tribe2) !== -1);
+  
+  assert.equal(this.get('user.tribes.length'), 2);
+  assert.ok(this.get('user.tribes').indexOf(this.tribe2) !== -1);
   assert.ok(this.$().text().includes(successSentence));
-  assert.ok(this.$().text().includes('Bienvenue chez : tribe2, tribe0 !'));
+
+  assert.ok(this.$().text().includes('Bienvenue chez : tribe3, tribe1 !'));
 });
 
 test('a user accepts its default tribe', function(assert) {
-  this.render(hbs`{{challenge-1 model=model}}`);
+  this.render(hbs`{{challenge-1 user=user challenges=challenges}}`);
 
   this.$('.tribeOK').click();
   assert.ok(this.$().text().includes(successSentence));
 });
 
 test('a user whith no tribe start with the choice screen', function(assert) {
-  this.get('model').user.set('tribes', []);
-  this.render(hbs`{{challenge-1 model=model}}`);
-
-  assert.ok(this.$().text().includes(choiceSentence));
+  let self = this;
+  Ember.run(function(){
+    self.set('user.tribes', []);
+    self.render(hbs`{{challenge-1 user=user challenges=challenges}}`);
+    assert.ok(self.$().text().includes(choiceSentence));
+  });
 });
 
 test('a user who has allready done the challenge start with the succeed screen', function(assert) {
-  this.get('model').user.set('challenges', [Ember.Object.create({ name: 'the tribes' })]);
-  this.render(hbs`{{challenge-1 model=model}}`);
+  this.get('user.challenges').setObjects([make('challenge', {name: 'the tribes'})]);
+  this.render(hbs`{{challenge-1 user=user challenges=challenges}}`);
   assert.ok(this.$().text().includes(successSentence));
 });
 
 test('a user who has allready done another challenge start with the intro screen', function(assert) {
-  this.get('model').user.set('challenges', [Ember.Object.create({ name: 'another' })]);
-  this.render(hbs`{{challenge-1 model=model}}`);
+  this.get('user.challenges').setObjects([make('challenge', {name: 'another'})]);
+  this.render(hbs`{{challenge-1 user=user challenges=challenges}}`);
   assert.ok(this.$().text().includes(introSentence));
 });
