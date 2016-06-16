c8edd7cf09425e7ed503a9a26a38397b7e5197bb
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-16 16:24:08 +0200

    Client Challenge summary: add link to redo a challenge

--- client/app/components/challenge-card.js
+++ client/app/components/challenge-card.js
@@ -4,7 +4,9 @@ export default Ember.Component.extend({
   done: Ember.computed('challenges', function() {
     return !!this.get('challenges').contains(this.get('challenge'));
   }),
-  click() {
-    this.get('onClick')(this.get('challenge'));
+  actions: {
+    selectChallenge() {
+      this.get('onClick')(this.get('challenge'));
+    }
   }
 });
