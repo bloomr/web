d8dc297df0599af6b7a16c060ba1409c14433900
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-18 22:26:19 +0200

    Client book-item: remove useless div

--- client/app/components/book-item.js
+++ client/app/components/book-item.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    click: function() {
-      this.get('onClick')(this.get('book'));
-    }
+  click: function() {
+    this.get('onClick')(this.get('book'));
   }
 });
