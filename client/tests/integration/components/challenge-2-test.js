fbc6d130f6525e160ca1a219095aafdf474f1c0b
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-19 15:13:24 +0200

    Client : simplify challenge2 test

--- client/tests/integration/components/challenge-2-test.js
+++ client/tests/integration/components/challenge-2-test.js
@@ -1,16 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
-import { make } from 'ember-data-factory-guy';
+import { make, manualSetup } from 'ember-data-factory-guy';
+import sinon from 'sinon';
 
 moduleForComponent('challenge-2', 'Integration | Component | challenge 2', {
   integration: true,
   beforeEach() {
+    manualSetup(this.container);
     this.set('user', make('user'));
     this.set('challenges', []);
-    let promiseWrapper = { promise: $.Deferred() };
-    this.register('service:book-search', Ember.Service.extend({ search() { return promiseWrapper.promise; } }));
-    this.promiseWrapper = promiseWrapper;
+    this.searchStub = sinon.stub(this.container.lookup('service:book-search'), 'search');
   }
 });
 
@@ -19,11 +19,12 @@ test('I can search a book and add it to my collections', function(assert) {
 
   this.$('input').val('super book');
   
+  let promise = $.Deferred();
+  this.searchStub.returns(promise);
   this.$('a.search').click();
   assert.ok(this.$().text().includes("on cherche dans l'internet"));
 
-  let self = this;
-  Ember.run(function(){ self.promiseWrapper.promise.resolve([{title: 'titre: un super book'}]); });
+  Ember.run(function(){ promise.resolve([{title: 'titre: un super book'}]); });
 
   assert.ok(this.$().text().includes('titre: un super book'));
   assert.notOk(this.$().text().includes("on cherche dans l'internet"));
@@ -42,17 +43,15 @@ test('I can search a book and another one', function(assert) {
   this.render(hbs`{{challenge-2 user=user}}`);
 
   this.$('input').val('super book');
-  let self = this;
+  this.searchStub.returns({ then(callback) { callback([{title: 'titre: un super book'}]); }});
   this.$('a.search').click();
-  Ember.run(function(){ self.promiseWrapper.promise.resolve([{title: 'titre: un super book'}]); });
   this.$('.title').click();
 
   this.$('.oneAgain').click();
-
-  this.promiseWrapper.promise = $.Deferred();
   this.$('input').val('tip top');
+
+  this.searchStub.returns({ then(callback) { callback([{title: 'titre: tip top'}]); }});
   this.$('a.search').click();
-  Ember.run(function(){ self.promiseWrapper.promise.resolve([{title: 'titre: tip top'}]); });
   this.$('.title').click();
 
   assert.ok(this.$().text().includes('titre: un super book'));
