3ef53bb62963f27b6e607321bd9cbd7a5ca79fbe
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-30 12:37:09 +0200

    Enrollment: user are now created and reroute to me/

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -5,14 +5,28 @@ class EnrollmentController < ApplicationController
   end
 
   def create
-    user_hash = params['user']
-    EnrollmentMailer.enroll_email(user_hash).deliver_now
-    Mailchimp.send_notif_about_bloomeur(
-      user_hash[:email], user_hash[:first_name], user_hash[:job_title])
-    redirect_to enrollment_thanks_path
+    user = User.create!(user_params)
+    notif(user_params)
+    sign_in(user)
+    redirect_to me_path
+  rescue StandardError => e
+    flash[:errors] = e.message
+    redirect_to enrollment_index_path
   end
 
   def thanks
     render layout: 'home'
+  end
+
+  private
+
+  def notif(user_params)
+    Mailchimp.send_notif_about_bloomeur(
+      user_params[:email], user_params[:first_name], user_params[:job_title]
+    )
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :password, :job_title)
   end
 end
--- spec/controllers/enrollment_controller_spec.rb
+++ spec/controllers/enrollment_controller_spec.rb
@@ -16,24 +16,41 @@ RSpec.describe EnrollmentController, type: :controller do
   end
 
   describe 'POST #create' do
-    before do
-      expect(Mailchimp).to receive(:send_notif_about_bloomeur)
-        .with('loulou@lou.com', 'loulou', 'job')
-      post :create, user: {
-        email: 'loulou@lou.com', job_title: 'job', first_name: 'loulou' }
+    context 'when all parameters are sent' do
+      before do
+        expect(Mailchimp).to receive(:send_notif_about_bloomeur)
+        expect(controller).to receive(:sign_in)
+        post :create, user: {
+          email: 'loulou@lou.com', job_title: 'job',
+          first_name: 'loulou', password: 'loulou12'
+        }
+      end
+
+      it 'returns redirect to enrollement_thanks' do
+        expect(response).to redirect_to(me_path)
+      end
+
+      it 'creates the user' do
+        user = User.find_by(email: 'loulou@lou.com')
+        expect(user.first_name).to eq('loulou')
+        expect(user.job_title).to eq('job')
+        expect(user.valid_password?('loulou12')).to be(true)
+      end
     end
 
-    it 'returns redirect to enrollement_thanks' do
-      expect(response).to redirect_to(enrollment_thanks_path)
-    end
+    context 'when the password is missing' do
+      before do
+        expect(Mailchimp).not_to receive(:send_notif_about_bloomeur)
+        post :create, user: {
+          email: 'loulou@lou.com', job_title: 'job',
+          first_name: 'loulou'
+        }
+      end
 
-    it 'send a link to typeform' do
-      mail = ActionMailer::Base.deliveries.last
-      real_typeform_link = mail.to_s[/(https:\/\/bloomr.*)" target/m, 1]
-                           .gsub("\r\n", '')
-      typeform_link = 'https://bloomr.typeform.com/to/FIuafU?email=3Dloulou@lo=u.com&amp;metier=3Djob&amp;prenom=3Dloulou'
-      expect(real_typeform_link).to eq typeform_link
-      expect(mail['to'].value).to eq('loulou@lou.com')
+      it 'redirects to index with an error' do
+        expect(response).to redirect_to(enrollment_index_path)
+        expect(flash[:errors]).to be
+      end
     end
   end
 end
