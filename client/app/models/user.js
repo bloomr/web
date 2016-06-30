840aa638dcfb9b459f74c001e3ede8e78d5213a3
Simon Lehericey <simon.lehericey@gmail.com>
2016-06-30 18:57:42 +0200

    Client: add user.isPhotoUploaded

--- client/app/models/user.js
+++ client/app/models/user.js
@@ -1,6 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
+import Ember from 'ember';
 
 export default Model.extend({
   jobTitle: attr('string'),
@@ -10,5 +11,8 @@ export default Model.extend({
   stats: attr(),
   questions: hasMany('question'),
   challenges: hasMany('challenge'),
-  avatarUrl: attr('string')
+  avatarUrl: attr('string'),
+  isPhotoUploaded: Ember.computed('avatarUrl', function() {
+    return this.get('avatarUrl') !== 'missing_thumb.png';
+  }),
 });
--- client/tests/unit/models/user-test.js
+++ client/tests/unit/models/user-test.js
@@ -1,4 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+import { makeList, manualSetup } from 'ember-data-factory-guy';
 
 moduleForModel('user', 'Unit | Model | user', {
   // Specify the other units that are required for this test.
@@ -7,6 +9,15 @@ moduleForModel('user', 'Unit | Model | user', {
 
 test('it exists', function(assert) {
   let model = this.subject();
-  // let store = this.store();
   assert.ok(!!model);
+});
+
+test('isPhotoUploaded is false initially', function(assert) {
+  Ember.run(() => this.subject().set('avatarUrl', 'missing_thumb.png'));
+  assert.notOk(this.subject().get('isPhotoUploaded'));
+});
+
+test('isPhotoUploaded is true if the user change its photo', function(assert) {
+  Ember.run(() => this.subject().set('avatarUrl', 'toto.png'));
+  assert.ok(this.subject().get('isPhotoUploaded'));
 });
