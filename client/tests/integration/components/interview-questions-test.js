809839aea8eb1fc9b0759488e01b14590d7703db
Simon Lehericey <simon.lehericey@gmail.com>
2016-07-01 18:08:33 +0200

    Client: fix integration tests

--- client/tests/integration/components/interview-questions-test.js
+++ client/tests/integration/components/interview-questions-test.js
@@ -1,21 +1,19 @@
-import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { make, makeList, manualSetup } from 'ember-data-factory-guy';
 
 moduleForComponent('interview-questions', 'Integration | Component | interview questions', {
   integration: true,
   beforeEach() {
-    this.user = Ember.Object.create({ jobTitle: 'job', questions: [{ title: 't1', answer: 'r1'  }]  });
+    manualSetup(this.container);
+    this.questions = makeList('question', 2);
+    this.user = make('user', { questions: this.questions });
   }
 });
 
 //TODO complete
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{interview-questions user=user}}`);
 
-  assert.equal(this.$('.job_title').val(), 'job');
-
+  assert.equal(this.$('.job_title').val(), 'jobTitle');
 });
