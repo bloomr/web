60656e13ea6af0b0e158f89f6dff4221d1956fc0
Simon Lehericey <simon.lehericey@gmail.com>
2016-07-06 10:17:23 +0200

    Client move profile introduction to challenges

--- client/app/components/profile-introduction.js
+++ client/app/components/profile-introduction.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['profile-introduction'],
   classNameBindings: ['hidden'],
   hidden:  Ember.computed('user.challenges.@each', function() {
     return !!this.get('user.challenges').findBy('name', 'interview');
