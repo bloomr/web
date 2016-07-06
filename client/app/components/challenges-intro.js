f1ebd97847a0e01662a67167d42c33765e5d3008
Simon Lehericey <simon.lehericey@gmail.com>
2016-07-06 10:19:20 +0200

    Client: add challenges intro

---
+++ client/app/components/challenges-intro.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNameBindings: ['hidden'],
+  hidden:  Ember.computed('user.challenges.@each', function() {
+    return !this.get('user.challenges').findBy('name', 'interview');
+  })
+});
