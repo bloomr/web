58995182f72f75b8f9b0456f3ddc5b703fb48c2e
Simon Lehericey <simon.lehericey@gmail.com>
2016-09-15 12:59:42 +0200

    New header: style and js

--- app/assets/javascripts/new_header.js
+++ app/assets/javascripts/new_header.js
@@ -1,4 +1,6 @@
 (function() {
+  var isMobile = ($(window).width() <= 480);
+
   var makeNavReadable = function (ev){
     if (window.pageYOffset>430) {
       $('nav').css('background-color','#ee933f');
@@ -7,7 +9,10 @@
     }
   };
 
+  var toogleMobileMenu = function() { $('nav > ul').toggle(); };
   var hideSubmenus = function() { $('.submenu').hide(); };
+
+  var exitTimer;
 
   var pageChange = function() {
     if ($('.new_header').length === 0) { return; }
@@ -21,20 +26,42 @@
       $(localSubmenus).hide();
     };
 
+    var toggleOnlySubmenu = function($submenu) {
+      var localSubmenus = $('.submenu').slice();
+      localSubmenus.splice(localSubmenus.index($submenu), 1);
+      $submenu.toggle();
+      $(localSubmenus).hide();
+    };
+
     var menuClick = function(e) {
       e.stopPropagation();
       var $target = $('.submenu', e.target.parentNode);
-      showOnlySubmenu($target);
+      toggleOnlySubmenu($target);
     };
-    $navHeaders.click(menuClick);
 
     var menuHover = function(e) {
+      clearTimeout(exitTimer);
       var $target = $('.submenu', e.target.parentNode);
       showOnlySubmenu($target);
     };
-    $navHeaders.hover(menuHover);
 
-    window.addEventListener('scroll', makeNavReadable, false);
+    var hideAfter = function () {
+      clearTimeout(exitTimer);
+      exitTimer = setTimeout(hideSubmenus, 3000);
+    };
+
+    var clearExitTimer = function () {
+      clearTimeout(exitTimer);
+    };
+
+    if(isMobile) {
+      $('.top .menu').click(toogleMobileMenu);
+    } else {
+      $navHeaders.hover(menuHover, hideAfter);
+      $('.submenu').hover(clearExitTimer, hideAfter);
+      window.addEventListener('scroll', makeNavReadable, false);
+    }
+    $navHeaders.click(menuClick);
     $('body').click(hideSubmenus);
   };
 
@@ -43,8 +70,11 @@
     $('body').off('click', hideSubmenus);
     $('header nav > ul > li > a').off();
     window.removeEventListener('scroll', makeNavReadable, false);
+    $('.top .menu').off();
+    $('.submenu').off();
   };
 
   $(document).on('page:change', pageChange);
   $(document).on('page:before-unload', beforeUnload);
+
 })();
