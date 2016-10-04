40a001bc9a0eb669e04170a147da27ef0e01e500
Simon Lehericey <simon.lehericey@gmail.com>
2016-10-04 16:43:23 +0200

    Test: Fix payment spec

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -1,25 +1,21 @@
 require 'rails_helper'
 
 feature 'Buy a journey' do
-
   before do
-    FactoryGirl.create(:campaign, :partner => 'default', :price => '35.0')
-    FactoryGirl.create(:campaign, :partner => 'sujetdubac', :price => '13.0')
+    FactoryGirl.create(:campaign, partner: 'default', price: '35.0')
+    FactoryGirl.create(:campaign, partner: 'sujetdubac', price: '13.0')
   end
 
   scenario 'Usually, I pay the full price' do
-
     visit '/payment'
-    expect(page).to have_button('Régler mes 35.00 €')
+    expect(page).to have_button('Payer 35.00 €')
     expect(page).to have_css('a[href="https://paypal.me/bloomr/35.00"]')
   end
 
   scenario 'I get a discount if I come from a partner' do
-
     visit '/partner/sujetdubac'
     visit '/payment'
-    expect(page).to have_button('Régler mes 13.00 €')
+    expect(page).to have_button('Payer 13.00 €')
     expect(page).to have_css('a[href="https://paypal.me/bloomr/13.00"]')
   end
 end
-
