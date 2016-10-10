0fba5e51eae49ee879dac2a16b1dee3e6c5d5900
Simon Lehericey <simon.lehericey@gmail.com>
2016-10-10 18:28:47 +0200

    New Style: change and replace team

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,10 @@
 class StaticController < ApplicationController
-  def qui_sommes_nous
-  end
-
   def templates
     render layout: 'home'
   end
 
-  %w( home program bred press concept bloomifesto ).each do |name|
+  %w( home program bred press concept
+      bloomifesto qui_sommes_nous ).each do |name|
     define_method(name) { render layout: 'new_home' }
   end
 end
