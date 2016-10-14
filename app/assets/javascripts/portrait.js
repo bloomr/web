1fb12b29cbe89a9c57302c661b04c31a59fac64e
Simon Lehericey <simon.lehericey@gmail.com>
2016-10-14 22:12:56 +0200

    Portrait: new style

--- app/assets/javascripts/portrait.js
+++ app/assets/javascripts/portrait.js
@@ -11,7 +11,7 @@
       sections: {
         work: $('#my-work'),
         books: $('#my-books'),
-        interview: $('#portrait_details')
+        interview: $('#my-interview')
       }
     };
 
--- app/controllers/portraits_controller.rb
+++ app/controllers/portraits_controller.rb
@@ -1,21 +1,26 @@
 class PortraitsController < ApplicationController
-
   def show
-    @portrait = User.published.includes(:questions).merge(Question.published).find(params[:id])
+    @portrait = User.published
+                    .includes(:tribes)
+                    .includes(:questions)
+                    .merge(Question.published)
+                    .find(params[:id])
+
     impressionist(@portrait)
     @popular_keywords = Keyword.popular_keywords
+
+    render layout: 'new_home'
   end
 
   def next
-    redirect_to action: "show", id: (User.next params[:id].to_i).id
+    redirect_to action: 'show', id: (User.next params[:id].to_i).id
   end
 
   def previous
-    redirect_to action: "show", id: (User.previous params[:id].to_i).id
+    redirect_to action: 'show', id: (User.previous params[:id].to_i).id
   end
 
   def aleatoire
-    redirect_to action: "show", id: (User.random).id
+    redirect_to action: 'show', id: User.random.id
   end
-
 end
