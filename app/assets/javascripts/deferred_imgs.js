aae703f4d03c812ac6c4f3758fedc8517849b1a4
Simon Lehericey <simon.lehericey@gmail.com>
2016-11-08 17:27:23 +0100

    Deffered imgs : works the first time

--- app/assets/javascripts/deferred_imgs.js
+++ app/assets/javascripts/deferred_imgs.js
@@ -1,5 +1,8 @@
 (function() {
   var init = function() {
+    // if its the first called by turbolinks, the dom is not ready
+    // load images latter
+    if (document.readyState !== 'complete') { return;  }
     var imgDefer = document.getElementsByTagName('img');
     for (var i=0; i<imgDefer.length; i++) {
       if(imgDefer[i].getAttribute('data-src')) {
@@ -8,4 +11,5 @@
     } 
   };
   document.addEventListener('turbolinks:load', init);
+  window.onload = init;
 })()
