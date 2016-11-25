93e5409d789f5e924975eaae22edad6691d65899
Simon Lehericey <simon.lehericey@gmail.com>
2016-11-25 10:50:39 +0100

    Cleaning: remove enrollment mailer

--- app/mailers/enrollment_mailer.rb
+++
@@ -1,7 +0,0 @@
-class EnrollmentMailer < ApplicationMailer
-
-  def enroll_email(user_hash)
-    @typeform_link = "https://bloomr.typeform.com/to/FIuafU?email=#{ user_hash['email'] }&metier=#{ user_hash['job_title'] }&prenom=#{ user_hash['first_name'] }"
-    mail(to: user_hash['email'], subject: 'Témoigner sur Bloomr')
-  end
-end
