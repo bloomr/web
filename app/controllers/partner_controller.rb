695e8e2ae06d8b24edac5784907a0b23f16d2adf
Simon Lehericey <simon.lehericey@gmail.com>
2016-12-12 14:54:18 +0100

    Cleaning

--- app/controllers/partner_controller.rb
+++ app/controllers/partner_controller.rb
@@ -1,10 +1,8 @@
 class PartnerController < ApplicationController
-
   def set_campaign
     campaign = Campaign.find_by_partner(params[:name])
     return redirect_to root_path if campaign.nil?
     cookies['partner'] = { value: params[:name], expires: 30.day.from_now }
     redirect_to campaign.campaign_url
   end
-
 end
