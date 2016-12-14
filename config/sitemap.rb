6c8db79537f36e3ef20b0fb5c2c61975b7e1a53d
Simon Lehericey <simon.lehericey@gmail.com>
2016-12-14 14:56:49 +0100

    Sitemap: handle blank tag case

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -21,7 +21,7 @@ SitemapGenerator::Sitemap.create do
                 .group(:keyword_id)
                 .having('count(keyword_id) > 2')
                 .pluck('keyword_id')
-  Keyword.find(keyword_ids).each { |k| add(tag_path(k.normalized_tag)) }
+  Keyword.find(keyword_ids).each { |k| add(tag_path(k.normalized_tag)) unless k.normalized_tag.blank? }
 
   one_pages = %w(press concept bloomifesto qui_sommes_nous testimonies program)
   one_pages.each { |path| add(send(path + '_path')) }
