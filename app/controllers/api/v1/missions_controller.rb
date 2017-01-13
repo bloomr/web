a9e54dd6dee33f32bec704df21f4504b046b0cac
Simon Lehericey <simon.lehericey@gmail.com>
2017-01-13 14:19:47 +0100

    Missions Controller: add tests

--- app/controllers/api/v1/missions_controller.rb
+++ app/controllers/api/v1/missions_controller.rb
@@ -2,9 +2,17 @@ module Api
   module V1
     class MissionsController < ApplicationController
       skip_before_filter :verify_authenticity_token
+      acts_as_token_authentication_handler_for Bloomy, fallback: :none
+      before_action :check_bloomy
+
       def create
-        mission = Mission.find_or_create_by(create_params)
-        render json: mission
+        render json: Mission.find_or_create_by(create_params)
+      end
+
+      private
+
+      def check_bloomy
+        render json: {}, status: :unauthorized if current_bloomy.nil?
       end
 
       def create_params
