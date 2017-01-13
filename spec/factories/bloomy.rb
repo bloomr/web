29facfec50a1556a199bf3ccdb521aef9e9993b5
Simon Lehericey <simon.lehericey@gmail.com>
2017-01-13 13:32:19 +0100

    Factory: bloomy

--- spec/factories/bloomy.rb
+++ spec/factories/bloomy.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :bloomy do
     first_name 'bloomy'
-    email 'bloomy@b.com'
+    sequence(:email) { |n| "bloomy#{n}@b.com" }
     age 21
     password 'bloomy12'
   end
