15c94d5de039cb1c12d4a6f260930033328921a4
Simon Lehericey <simon.lehericey@gmail.com>
2017-02-10 13:23:25 +0100

    Bloomies Controller : add test on expected json

--- spec/controllers/api/v1/bloomies_controller_spec.rb
+++ spec/controllers/api/v1/bloomies_controller_spec.rb
@@ -5,15 +5,62 @@ RSpec.describe Api::V1::BloomiesController, type: :controller do
   let!(:bloomy2) { create(:bloomy) }
   let!(:mission) { Mission.create(prismic_id: '1') }
   let!(:mission2) { Mission.create(prismic_id: '2') }
+  let!(:program) do
+    program = Program.create(name: 'program', discourse: true, intercom: true)
+    program.missions << mission
+    bloomy.programs << program
+    program
+  end
+
+  let(:body) { JSON.parse(response.body) }
+
+  let(:expected_json) do
+    { 'data' => {
+      'id' => '1',
+      'type' => 'bloomies',
+      'attributes' => {
+        'first-name' => 'bloomy',
+        'email' => 'bloomy5@b.com'
+      },
+      'relationships' => {
+        'programs' => { 'data' => [{ 'id' => '1', 'type' => 'programs' }] }
+      }
+    },
+      'included' => [
+        { 'id' => '1',
+          'type' => 'programs',
+          'attributes' => {
+            'name' => 'program',
+            'discourse' => true,
+            'intercom' => true
+          },
+          'relationships' => {
+            'missions' => {
+              'data' => [{ 'id' => '1', 'type' => 'missions' }]
+            }
+          } },
+        { 'id' => '1',
+          'type' => 'missions',
+          'attributes' => { 'prismic-id' => '1' } }
+      ] }
+  end
 
   describe 'show' do
     context 'when the bloomy is logged' do
-      it 'returns the logged user' do
+      before :each do
         get :show, id: bloomy.id,
                    'bloomy_email': bloomy.email,
                    'bloomy_token': bloomy.authentication_token
+      end
 
+      it 'returns the logged user' do
         expect(response).to have_http_status(:success)
+      end
+
+      describe 'the response' do
+        it 'includes programs and missions' do
+          expect(body).to eq(expected_json)
+        end
       end
 
       it 'returns unauthorized if another bloomy is required' do
@@ -32,5 +79,4 @@ RSpec.describe Api::V1::BloomiesController, type: :controller do
       end
     end
   end
-
 end
