d1e5eddce57e3d95c12274a137d09749d86aa826
Simon Lehericey <simon.lehericey@gmail.com>
2017-03-17 18:35:41 +0100

    Bouygues: program page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   %w( home program bred press concept
-      bloomifesto qui_sommes_nous ).each do |name|
+      bloomifesto qui_sommes_nous bouygues ).each do |name|
     define_method(name) {}
   end
 end
